0fc6e94b228b77d49b21b6b0c1e242c6a400fb09
Nguyễn Văn Tín <bguyt46@gmail.com>
2026-03-06 22:07:15 +0700

    chore(api): add item-events schema and update seed sql

---
+++ apps/api/src/db/schemas/events/item-events.ts
@@ -0,0 +1,34 @@
+import { sql } from 'drizzle-orm'
+import { sqliteTable, text, integer, primaryKey } from 'drizzle-orm/sqlite-core'
+import { users } from '../users'
+
+export const itemEvents = sqliteTable(
+    'item_events',
+    {
+        id: text('id').notNull().primaryKey(),
+        name: text('name').notNull(),
+        logo: text('logo').notNull(),
+        description: text('description').notNull(),
+        linkSocial: text('link_social', { mode: 'json' })
+            .$type<{
+                type: 'twitter' | 'facebook' | 'instagram' | 'tiktok' | 'youtube' | 'other';
+                url: string;
+                handle: string;
+                isPublic: boolean
+            }>()
+            .notNull(),
+        level: integer('level').default(0),
+        parentId: text('parent_id'),
+        type: text('type', {
+            enum: ['player', 'team', 'tournament'],
+        }).notNull(),
+        createdBy: text('created_by')
+            .notNull()
+            .references(() => users.id, { onDelete: 'cascade' }),
+        createdAt: text('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+        updatedAt: text('updated_at').notNull(),
+    },
+    (t) => ({
+        pk: primaryKey({ columns: [t.id] }),
+    })
+)
---
+++ apps/api/src/db/schemas/events/pickem-events.ts
@@ -0,0 +1,63 @@
+import { sql } from 'drizzle-orm'
+import { sqliteTable, text, integer, primaryKey } from 'drizzle-orm/sqlite-core'
+import { users } from '../users'
+import { itemEvents } from './item-events'
+
+export const pickemEvents = sqliteTable(
+    'pickem_events',
+    {
+        id: text('id').notNull().primaryKey(),
+        title: text('title').notNull(),
+        thumbnail: text('thumbnail').notNull(),
+        description: text('description').notNull(),
+        winPoints: integer('win_points').notNull(),
+        pickPoints: integer('pick_points').notNull(),
+        winExp: integer('win_exp').notNull(),
+        pickExp: integer('pick_exp').notNull(),
+        eventDate: text('event_date').notNull(),
+        createdBy: text('created_by')
+            .notNull()
+            .references(() => users.id, { onDelete: 'cascade' }),
+        createdAt: text('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+        updatedAt: text('updated_at').notNull(),
+    },
+    (t) => ({
+        pk: primaryKey({ columns: [t.id] }),
+    })
+)
+export const pickemEventOptions = sqliteTable(
+    'pickem_event_options',
+    {
+        id: text('id').notNull().primaryKey(),
+        eventId: text('event_id')
+            .notNull()
+            .references(() => pickemEvents.id, { onDelete: 'cascade' }),
+        eventItemId: text('event_item_id')
+            .notNull()
+            .references(() => itemEvents.id, { onDelete: 'cascade' }),
+        isWinningOption: integer('is_winning_option').notNull().default(0),
+
+    },
+    (t) => ({
+        pk: primaryKey({ columns: [t.id] }),
+    })
+)
+export const pickemEventPicks = sqliteTable(
+    'pickem_event_picks',
+    {
+        id: text('id').notNull().primaryKey(),
+        userId: text('user_id')
+            .notNull()
+            .references(() => users.id, { onDelete: 'cascade' }),
+        eventId: text('event_id')
+            .notNull()
+            .references(() => pickemEvents.id, { onDelete: 'cascade' }),
+        optionId: text('option_id')
+            .notNull()
+            .references(() => pickemEventOptions.id, { onDelete: 'cascade' }),
+        pickedAt: text('picked_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+    },
+    (t) => ({
+        pk: primaryKey({ columns: [t.id] }),
+    })
+)
